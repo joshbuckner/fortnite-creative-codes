5797ce955c5ec78e4eb0677a86554a8c1f20c771
Josh Buckner <joshgbuckner@gmail.com>
2019-01-14 03:06:41 -0500

    added map submission form, fixed map search

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("btn-container");
+var btns = btnContainer.getElementsByClassName("btn-filter");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
+
 filterSelection("all")
 
 function filterSelection(c) {
@@ -13,15 +24,16 @@ function filterSelection(c) {
 
 function filterByMapName(c) {
     for (var i = 0; i < btns.length; i++) {
-      var current = document.getElementsByClassName("active");
-          current[0].className = current[0].className.replace(" active", "");
-          btns[btns.length-1].className += " active";
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        btns[btns.length - 1].className += " active";
     }
 
     if (c == "all") c = "";
     for (var i = 0; i < $(".filter-div .card-header").length; i++) {
         removeClass($(".filter-div")[i], "show");
-        if ($(".filter-div .card-header")[i].innerHTML.toLowerCase() === $("#search-input")[0].value.toLowerCase()) {
+        if ($(".filter-div .card-header")[i].innerHTML.toLowerCase().includes($("#search-input")[0].value.toLowerCase()) && $("#search-input")[0].value.toLowerCase() !== '') {
+        // if ($(".filter-div .card-header")[i].innerHTML.toLowerCase() === $("#search-input")[0].value.toLowerCase()) {
             addClass($(".filter-div")[i], "show");
         }
     }
@@ -50,17 +62,6 @@ function removeClass(element, name) {
     element.className = arr1.join(" ");
 }
 
-// Add active class to the current button (highlight it)
-var btnContainer = document.getElementById("btn-container");
-var btns = btnContainer.getElementsByClassName("btn-filter");
-for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function() {
-        var current = document.getElementsByClassName("active");
-        current[0].className = current[0].className.replace(" active", "");
-        this.className += " active";
-    });
-}
-
 function searchCards() {
     filterByMapName($("#search-input")[0].value.toLowerCase());
     displaySearchHeading();
@@ -81,3 +82,34 @@ function displaySearchHeading() {
         document.getElementsByClassName("jumbotron-heading")[0].innerHTML = 'Results for "' + $("#search-input")[0].value + '"';
     }
 }
+
+function showSubmit() {
+  $(".submit-map")[0].classList.remove("hidden");
+}
+
+function hideSubmit() {
+  $(".submit-map")[0].classList.add("hidden");
+  console.log('hiding');
+}
+
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  'use strict'
+
+  window.addEventListener('load', function () {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation')
+
+    // Loop over them and prevent submission
+    Array.prototype.filter.call(forms, function (form) {
+      form.addEventListener('submit', function (event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+        form.classList.add('was-validated')
+      }, false)
+    })
+  }, false)
+}())
